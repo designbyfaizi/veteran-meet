f209461c82b3fb01b9457da6af6746fad0901610
designbyfaizi <faizanullah1999@gmail.com>
2022-05-21 21:26:54 +0500

    Added Community Event Modals

--- src/components/CommunityDashboard/Event.tsx
+++ src/components/CommunityDashboard/Event.tsx
@@ -8,21 +8,46 @@ interface EventProps {
   date: string;
 }
 
-const Event: FC<EventProps> = ({heading, description, hobby, date}) => {
+const Event: FC<EventProps> = ({ heading, description, hobby, date }) => {
   const [editOpened, setEditOpened] = useState(false);
   const [deleteOpened, setDeleteOpened] = useState(false);
+  const [eventOpened, setEventOpened] = useState(false);
+  const EventModal = () => (
+    <Modal
+      withCloseButton={false}
+      opened={eventOpened}
+      onClose={() => setEventOpened(false)}
+    >
+      <h1>Event Details Modal</h1>
+    </Modal>
+  );
   const EditModal = () => (
-    <Modal opened={editOpened} onClose={() => setEditOpened(false)}>
-      {/* Modal content */}
+    <Modal
+      withCloseButton={false}
+      opened={editOpened}
+      onClose={() => setEditOpened(false)}
+    >
+      <h1>Edit Event Modal</h1>
     </Modal>
   );
   const DeleteModal = () => (
-    <Modal opened={deleteOpened} onClose={() => setDeleteOpened(false)}>
-      {/* Modal content */}
+    <Modal
+      withCloseButton={false}
+      opened={deleteOpened}
+      onClose={() => setDeleteOpened(false)}
+    >
+      <h1>Delete Event Modal</h1>
     </Modal>
   );
   return (
-    <li className="event flex gap-10 items-center hover:bg-light-700 p-4 rounded-2xl">
+    <li
+      onClick={(event) => {
+        event.stopPropagation();
+        setEventOpened(true);
+      }}
+      className="event flex gap-10 items-center hover:bg-light-700 p-4 rounded-2xl cursor-pointer"
+    >
+      <EventModal />
       <EditModal />
       <DeleteModal />
       <img
@@ -32,23 +57,27 @@ const Event: FC<EventProps> = ({heading, description, hobby, date}) => {
       />
       <div className="flex flex-col gap-5 h-full justify-between w-full">
         <h2 className="eventheading text-[23px] font-normal">{heading}</h2>
-        <p className="text-[16px] font-normal">
-          {description}
-        </p>
+        <p className="text-[16px] font-normal">{description}</p>
         <div className="flex justify-between items-center">
           <h5 className="tag px-[14px] py-[6px] text-[15px] font-medium text-primary-lighter bg-primary-darker rounded-full">
             {hobby}
           </h5>
           <div className="flex gap-4">
             <Button
-              onClick={() => setEditOpened(true)}
+              onClick={(event: any) => {
+                event.stopPropagation();
+                setEditOpened(true);
+              }}
               radius={"md"}
               className="bg-[#F6F6F6] text-primary-darker hover:bg-primary-darker hover:text-white"
             >
               Edit
             </Button>
             <Button
-              onClick={() => setDeleteOpened(true)}
+              onClick={(event: any) => {
+                event.stopPropagation();
+                setDeleteOpened(true);
+              }}
               radius={"md"}
               className="bg-primary-darker text-white hover:bg-red-600"
             >
